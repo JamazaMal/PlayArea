45fc3dd877aeb1c2917bb4ea5a94e57a64f7144e
JamazaMal <36673082+JamazaMal@users.noreply.github.com>
2018-02-20 12:44:09 -0700

    Initial commit

---
+++ eu1.py
@@ -0,0 +1,16 @@
+import time
+
+
+def prime_sum(n):
+    pr = [True] * (n+1)
+    pr[0], pr[1] = [False] * 2
+    s = 0
+    for num, ip in enumerate(pr):
+        if ip:
+            s = s + num
+            pr[num*2::num] = [False] * (n//num-1)
+    return s
+
+
+st = time.time()
+print("{0} in {1} seconds".format(prime_sum(2000000), time.time()-st))
