7addb37ac6c6101f67403a54f8ac8bbe12bb412b
jamazamal <jamaza@outlook.com>
2018-02-20 22:17:05 -0700

    Ongoing

--- eu012.py
+++ eu012.py
@@ -1,26 +1,37 @@
 import time
-
-
-def tri(n):
-    return n * (n+1) / 2
+mx = 15000  # Random Search depth limit
+divs = [0] * (mx)
 
 
 def cdiv(n):
+    global divs
+    if divs[n] != 0:
+        return divs[n]
+
     c = 2
-    for i in range(2, (int(n**0.5))):
+    for i in range(2, int((n-1)**0.5)):
         if n % i == 0:
             c = c + 2
-    if int(n**0.5) == (n**0.5):
+    if (n**0.5).is_integer():
         c = c + 1
+    divs[n] = c
     return c
 
 
 def main():
     st = time.time()
-    for i in range(7, 100000):
-        z = tri(i)
-        x = cdiv(z)
-        if x > 500:
-            print("{0} - {1} - {2} - {3} seconds.".format(i, int(z), x, time.time() - st))
-            break
+    for n in range(3, mx):
+        if n % 2 == 0:
+            x = cdiv(n//2) * cdiv(n+1)
+            if x > 500:
+                break
+        else:
+            x = cdiv(n) * cdiv((n+1)//2)
+            if x > 500:
+                break
+
+    print("Found {0} in {1} seconds.".format(n * (n+1) / 2, time.time() - st))
+
+
+main()
 
