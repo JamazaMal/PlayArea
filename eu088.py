4fbef4f0be50b33e50b46f25b2cad2904c5a78c6
jmalan <jamaza>
2019-02-21 08:17:41 -0700

    dfd

--- eu088.py
+++ eu088.py
@@ -1,21 +1,19 @@
-import itertools as it
+mk = 12000
+res = [100000] * mk
 
-
-def prod(_i):
-    ret = 1
-    for i in _i:
-        ret = ret * i
-    return ret
-
-
-def k(_i):
-    p = prod(_i)
-    l = p - sum(_i)
-    return [i for i in it.chain([1] * l, _i)], l+len(_i), p
+def nextFact(pd, sm, fc, lf):
+    k = pd - sm + fc
+    if k < mk:
+        if pd < res[k]:
+            res[k] = pd
+        for i in range(lf, mk//pd*2+1):
+            nextFact(pd*i, sm+i, fc+1, i)
 
 
 def main():
-    print(k([2, 2, 2]))
+    nextFact(1, 1, 1, 2)
+    #print(res)
+    print(sum(set(res[2:])))
 
 
 if __name__ == "__main__":
