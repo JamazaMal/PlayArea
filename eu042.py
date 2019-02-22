1515b15e18fbc2b088043e151dd8541e36e430ec
JamazaMal <36673082+JamazaMal@users.noreply.github.com>
2019-02-22 08:24:38 -0700

    From work

--- eu042.py
+++ eu042.py
@@ -13,12 +13,9 @@ def main():
     f = open('042.txt', 'r')
     ret = 0
     nams = f.read().split(',')
-    print(len(nams))
     for n in nams:
         i = namScore(n)
         j = ((8*i)+1)**0.5
-        # print (n,i,j)
         if j == int(j):
-            print(n, i)
             ret = ret + 1
     print(ret)
---
+++ eu091.py
@@ -0,0 +1,8 @@
+def main():
+    mx = 51
+    for x1 in range(mx):
+        for x2 in range(mx):
+            for y1 in range(mx):
+                for y2 in range(mx):
+                    pass
+        print(x1)
